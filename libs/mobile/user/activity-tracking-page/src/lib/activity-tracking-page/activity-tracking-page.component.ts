102dd7c1dfb0cb19c87ebc35f78ef2cc84072a38
Brandon Humboldt <brhumboldt@gmail.com>
2024-04-22 16:48:45 -0500

    maplibre integration for displaying route when tracking activity

--- libs/mobile/user/activity-tracking-page/src/lib/activity-tracking-page/activity-tracking-page.component.ts
+++ libs/mobile/user/activity-tracking-page/src/lib/activity-tracking-page/activity-tracking-page.component.ts
@@ -1,24 +1,82 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButton } from '@angular/material/button';
-import { combineLatest, interval, map, Subject, takeUntil } from 'rxjs';
+import {
+  combineLatest,
+  interval,
+  map,
+  Observable,
+  Subject,
+  takeUntil,
+  tap,
+} from 'rxjs';
 import { Store } from '@ngrx/store';
 import {
   currentTrackingInfoSelector,
   TrackingActions,
 } from '@cockpit/mobile/tracking-state';
+import {
+  FeatureComponent,
+  GeoJSONSourceComponent,
+  LayerComponent,
+  MapComponent,
+} from '@maplibre/ngx-maplibre-gl';
 
 @Component({
   selector: 'cockpit-activity-tracking-page',
   standalone: true,
-  imports: [CommonModule, MatButton],
+  imports: [
+    CommonModule,
+    MatButton,
+    MapComponent,
+    FeatureComponent,
+    GeoJSONSourceComponent,
+    LayerComponent,
+  ],
   templateUrl: './activity-tracking-page.component.html',
   styleUrl: './activity-tracking-page.component.scss',
 })
 export class ActivityTrackingPageComponent {
   stopTracking$ = new Subject();
 
+  zoom: [number] = [18];
+  pitch: [number] = [30];
+
   trackedActivity$ = this._store.select(currentTrackingInfoSelector);
+  currentPosition$: Observable<[number, number]> = this.trackedActivity$.pipe(
+    map((activity) =>
+      activity && activity.locations && activity.locations.length
+        ? [activity.locations[0].long, activity.locations[0].lat]
+        : [0, 0]
+    )
+  );
+
+  geometry$: Observable<GeoJSON.FeatureCollection<GeoJSON.LineString>> =
+    this.trackedActivity$.pipe(
+      map((activity) => ({
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry:
+              activity && activity.locations && activity.locations.length
+                ? {
+                    type: 'LineString',
+                    coordinates: activity.locations.map((location) => [
+                      location.long,
+                      location.lat,
+                    ]),
+                  }
+                : {
+                    type: 'LineString',
+                    coordinates: [],
+                  },
+          },
+        ],
+      }))
+    );
+
   duration$ = combineLatest([
     interval(1000).pipe(takeUntil(this.stopTracking$)),
     this.trackedActivity$,
